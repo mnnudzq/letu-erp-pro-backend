96a7dabe8ff99ecad2ad9468c013a46f416ad86d
dengzhiqiang <zhiqiang.deng@fft.com.cn>
2020-07-06 19:42:30 +0800

    Revert "[dev] 构建项目-1"
    This reverts commit f632710ee28d0879d6610999282d236bcb6264e8.

--- src/main/java/com/jq/main/sche/SchedulerTask.java
+++
@@ -1,9 +0,0 @@
-package com.jq.main.sche;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SchedulerTask {
-
-}
