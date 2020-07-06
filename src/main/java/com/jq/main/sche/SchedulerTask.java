f632710ee28d0879d6610999282d236bcb6264e8
dengzhiqiang <zhiqiang.deng@fft.com.cn>
2020-07-06 18:50:31 +0800

    [dev] 构建项目-1

---
+++ src/main/java/com/jq/main/sche/SchedulerTask.java
@@ -0,0 +1,9 @@
+package com.jq.main.sche;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulerTask {
+
+}
